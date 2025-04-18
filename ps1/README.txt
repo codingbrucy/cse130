250a1693b52ca599aa9810dd22ed89fbb137ccd8
Bruce Ning <dning2@ucsc.edu>
2025-04-17 18:31:51 -0700

    hw1 submitted

--- ps1/README.txt
+++ ps1/README.txt
@@ -2,10 +2,16 @@
 
 Q1. fork() and exec()
 1. “Why This Works" explanation: 
-2. Testing strategy and inputs:
-3. Output analysis:
+fork() is called twice in a non-nested manner, thus spawnning two children, first one calls execlp() with first two 
+command line args, and the second child calls exevp with the third argv and the array represented by argv[3].
+then the program uses waitpid twice with both pids, getting out of a status. 
+
+2. Testing strategy and inputs: tested with two basic echo commands; then echo and one ls
+3. Output analysis: both commands executing success, reporting exit status success.
 
 Q2. Pipes
-1. “Why This Works" explanation: 
-2. Testing strategy and inputs:
-3. Output analysis:
+1. “Why This Works" explanation: fork() is chained 3 times. first child: routing output of ls to pipe1[1]; then in the 
+second child, xargs is executed after routing input from pipe1[0] and output to pipe2[1]; lastly the cute command reads
+from pipe2[0], printing to console. in the end parents reaps all 3 dead children and terminates
+2. Testing strategy and inputs: tested with ./pipe a b, with a, b being arbitrary ints. 
+3. Output analysis: the output is the equivalent of that of executing the target command to be implemented. 
--- ps1/pipes.c
+++ ps1/pipes.c
@@ -1,21 +1,113 @@
-#include <unistd.h>
+// pipes.c
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <sys/wait.h>
 #define READEND 0
 #define WRITEEND 1
 
-int main(int argc,char* argv[]) {  
-   if (argc < 3) {
+int main(int argc, char *argv[])
+{
+   if (argc < 3)
+   {
       printf("Usage: pipes a b, where a and b are integers. \n");
       exit(1);
    }
-   
-   /* TODO: Implement the command: "ls /dev | xargs | cut -d ' ' -f<a>-<b>" 
-      	    where <a> and <b> are integers that specify the range and are taken as input from the command line. */
 
+   /* TODO: Implement the command: "ls /dev | xargs | cut -d ' ' -f<a>-<b>"
+             where <a> and <b> are integers that specify the range and are taken as input from the command line. */
+   // Parse field range
+   int a = atoi(argv[1]);
+   int b = atoi(argv[2]);
+   if (a <= 0 || b <= 0 || a > b)
+   {
+      fprintf(stderr, "Error: <a> and <b> must be positive with a <= b\n");
+      return 1;
+   }
+   // Build the "-f<a>-<b>" argument
+   char field_arg[32];
+   snprintf(field_arg, sizeof(field_arg), "%d-%d", a, b);
+
+   // Create the two pipes
+   int pipe1[2], pipe2[2];
+   if (pipe(pipe1) < 0 || pipe(pipe2) < 0)
+   {
+      perror("pipe");
+      return 1;
+   }
+
+   pid_t pid;
+
+   // Stage 1: ls /dev
+   pid = fork();
+   if (pid < 0)
+   {
+      perror("fork");
+      return 1;
+   }
+   if (pid == 0)
+   {
+      // Child 1: stdout -> pipe1 write end
+      dup2(pipe1[1], STDOUT_FILENO);
+      close(pipe1[0]);
+      close(pipe1[1]);
+      close(pipe2[0]);
+      close(pipe2[1]);
+      execlp("ls", "ls", "/dev", (char *)NULL);
+      perror("execlp ls");
+      exit(1);
+   }
+
+   // Stage 2: xargs
+   pid = fork();
+   if (pid < 0)
+   {
+      perror("fork");
+      return 1;
+   }
+   if (pid == 0)
+   {
+      // Child 2: stdin <- pipe1 read end, stdout -> pipe2 write end
+      dup2(pipe1[0], STDIN_FILENO);
+      dup2(pipe2[1], STDOUT_FILENO);
+      close(pipe1[0]);
+      close(pipe1[1]);
+      close(pipe2[0]);
+      close(pipe2[1]);
+      execlp("xargs", "xargs", (char *)NULL);
+      perror("execlp xargs");
+      exit(1);
+   }
+
+   pid = fork();
+   if (pid < 0)
+   {
+      perror("fork");
+      return 1;
+   }
+   if (pid == 0)
+   {
+      // Child 3: stdin <- pipe2 read end
+      dup2(pipe2[0], STDIN_FILENO);
+      close(pipe1[0]);
+      close(pipe1[1]);
+      close(pipe2[0]);
+      close(pipe2[1]);
+      execlp("cut", "cut", "-d", " ", "-f", field_arg, (char *)NULL);
+      perror("execlp cut");
+      exit(1);
+   }
+
+   // Parent: close all pipe fds, then wait for children
+   close(pipe1[0]);
+   close(pipe1[1]);
+   close(pipe2[0]);
+   close(pipe2[1]);
+
+   for (int i = 0; i < 3; ++i)
+   {
+      wait(NULL);
+   }
 
    return 0;
 }
-
-
